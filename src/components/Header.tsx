6e0cd4271cb2ec5c22a213f6bdaa48848c8e5440
AndreiYantsevich <yantsevich92@gmail.com>
2021-07-21 13:36:28 +0300

    create components and changed css

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className={'header'}>
+            <img src="https://assets.turbologo.com/blog/en/2019/11/19084834/gaming-logo-cover.jpg" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
