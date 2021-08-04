dfc1b5dd6e703088128e9315e22b85a4caef206a
AndreiYantsevich <yantsevich92@gmail.com>
2021-08-04 12:49:14 +0300

    create state

---
+++ src/redux/state.ts
@@ -0,0 +1,48 @@
+export type MessageType = {
+    id: number
+    message: string
+}
+export type DialogType = {
+    id: number
+    name: string
+}
+export type PostsType = {
+    id: number
+    message: string
+    likesCount: number
+}
+export type ProfilePageType = {
+    posts: Array<PostsType>
+}
+export type DialogPageType = {
+    dialogs: Array<DialogType>
+    messages: Array<MessageType>
+}
+type RootStateType = {
+    profilePage: ProfilePageType
+    dialogsPage: DialogPageType
+}
+export let state: RootStateType = {
+    dialogsPage: {
+        dialogs : [
+            {id: 1, name: 'Andrei'},
+            {id: 2, name: 'Viktoria'},
+            {id: 3, name: 'Arseny'},
+            {id: 4, name: 'Visha'},
+            {id: 5, name: 'Tomas'}
+        ],
+        messages : [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'Agu!'},
+            {id: 4, message: 'Gav!'},
+            {id: 5, message: 'Meow!'}
+        ]
+    },
+    profilePage: {
+        posts : [
+            {id: 1, message: 'Hi, how are you?', likesCount: 25},
+            {id: 2, message: 'This is my first project', likesCount: 49}
+        ]
+    }
+}
