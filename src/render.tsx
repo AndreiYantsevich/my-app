b72ac5df8f60f68d19f8d9ba4a3ede992716e20b
AndreiYantsevich <yantsevich92@gmail.com>
2021-08-05 14:17:22 +0300

    add render.tsx

---
+++ src/render.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App} from './App';
+import {addPost, addMessage, RootStateType} from './redux/state';
+import {BrowserRouter} from 'react-router-dom';
+
+export let rerenderEntireTree = (state: RootStateType) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} addMessage={addMessage}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
