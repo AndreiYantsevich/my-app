4a77d8a2eedd8258aa0c12684c73522be3631d7b
Andrei <yantsevich92@gmail.com>
2021-12-27 13:28:15 +0300

    pure component

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {FC, memo} from 'react';
+import React, {FC} from 'react';
 import style from './MyPosts.module.css';
 import Post, {PostType} from './Post/Post';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
@@ -16,39 +16,48 @@ type FormDataType = {
 
 const maxLength10 = maxLengthCreator(10);
 
-const MyPosts: FC<PropsType> = memo((props) => {
+class MyPosts extends React.PureComponent<PropsType> {
 
-    const postsElements = props.posts.map(p => <Post id={p.id} message={p.message}
-                                                     likesCount={p.likesCount}/>)
+    /*shouldComponentUpdate(nextProps: PropsType, nextState: {}): boolean {
+        return nextProps !== this.props || nextState !== this.state;
+    }*/
 
-    const addNewPost = (formData: FormDataType) => {
-        props.addPost(formData.newPostText)
-    }
+    render() {
 
-    return (
-        <div className={style.postsBlock}>
-            <h3>My posts</h3>
-            <AddNewPostFormRedux onSubmit={addNewPost}/>
-            <div className={style.posts}>
-                {postsElements}
+        const postsElements = this.props.posts.map(p => <Post id={p.id}
+                                                              message={p.message}
+                                                              likesCount={p.likesCount}/>)
+
+        const addNewPost = (formData: FormDataType) => {
+            this.props.addPost(formData.newPostText)
+        }
+
+        return (
+            <div className={style.postsBlock}>
+                <h3>My posts</h3>
+                <AddNewPostFormRedux onSubmit={addNewPost}/>
+                <div className={style.posts}>
+                    {postsElements}
+                </div>
             </div>
-        </div>
-    );
-});
+        );
+    }
+};
 
-const AddNewPostForm: FC<InjectedFormProps<FormDataType>> = memo((props) => {
+const AddNewPostForm: FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
                 <Field component={Textarea} name={'newPostText'}
-                       placeholder={'Enter your post'} validate={[requiredField, maxLength10]}/>
+                       placeholder={'Enter your post'}
+                       validate={[requiredField, maxLength10]}/>
             </div>
             <div>
                 <button>Add post</button>
             </div>
         </form>
     );
-});
+};
 
 const AddNewPostFormRedux = reduxForm<FormDataType>({form: 'profileAddPostForm'})(AddNewPostForm)
 
